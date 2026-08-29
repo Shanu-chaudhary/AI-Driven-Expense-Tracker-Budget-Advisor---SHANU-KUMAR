5e61724ed2049a8e35f09821d22d63c8f607bb74
Shanu-chaudhary <beingshanu789@gmail.com>
2026-08-29 23:38:47 +0530

    Hello

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,25 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:8080/api", // your backend base URL
+// });
+
+// instance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token && token !== 'undefined' && token !== 'null') {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default instance;
+
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api", // your backend base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL 
+    ? `${import.meta.env.VITE_API_BASE_URL}/api` 
+    : "http://localhost:8080/api",
 });
 
 instance.interceptors.request.use((config) => {
